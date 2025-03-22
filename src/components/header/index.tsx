3b1acafdc9d2fa7cd98aea729ff9687c2ef639a8
CODE WEB <gabrielronaldo9343@gmail.com>
2025-03-22 11:13:01 -0400

    update: adicionado cores padão.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,20 +4,20 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white shadow-md sticky top-0 z-50">
+    <header className="bg-azul-7 text-white shadow-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
-        <h1 className="text-xl font-bold text-blue-600">SEPLAG</h1>
+        <h1 className="text-xl font-bold">SEPLAG</h1>
 
         {/* Menu Desktop */}
         <nav className="hidden md:flex space-x-6">
-          <a href="#" className="text-gray-700 hover:text-blue-600 transition">Home</a>
-          <a href="#" className="text-gray-700 hover:text-blue-600 transition">Sobre</a>
-          <a href="#" className="text-gray-700 hover:text-blue-600 transition">Contato</a>
+          <a href="#" className="hover:text-azul-3 transition">Home</a>
+          <a href="#" className="hover:text-azul-3 transition">Sobre</a>
+          <a href="#" className="hover:text-azul-3 transition">Contato</a>
         </nav>
 
         {/* Botão Menu Mobile */}
         <button
-          className="md:hidden text-gray-700 focus:outline-none"
+          className="md:hidden focus:outline-none"
           onClick={() => setMenuOpen(!menuOpen)}
         >
           <svg className="w-[50px]" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
@@ -32,13 +32,14 @@ const Header = () => {
 
       {/* Menu Mobile */}
       {menuOpen && (
-        <nav className="md:hidden bg-white border-t border-gray-200 px-4 py-2 space-y-2">
-          <a href="#" className="block text-gray-700 hover:text-blue-600 transition">Home</a>
-          <a href="#" className="block text-gray-700 hover:text-blue-600 transition">Sobre</a>
-          <a href="#" className="block text-gray-700 hover:text-blue-600 transition">Contato</a>
+        <nav className="md:hidden text-white border-t border-gray-200 px-4 py-2 space-y-2 text-azul-7">
+          <a href="#" className="block hover:text-azul-3 transition">Home</a>
+          <a href="#" className="block hover:text-azul-3 transition">Sobre</a>
+          <a href="#" className="block hover:text-azul-3 transition">Contato</a>
         </nav>
       )}
     </header>
+
   )
 }
 
